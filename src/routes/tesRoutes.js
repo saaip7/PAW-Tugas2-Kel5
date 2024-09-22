5e53b3748cf08b709b99b97b6de303807c27a1a2
melvinwaluyo <melvinwaluyo@mail.ugm.ac.id>
2024-09-22 20:04:01 +0700

    feat: add reviews

--- src/routes/tesRoutes.js
+++
@@ -1,9 +0,0 @@
-const tesRoutes = require("express").Router();
-
-//Define first
-const { tesCreate } = require("../controller/tesController");
-
-//All the CRUD disini
-tesRoutes.post("/tesCreate", tesCreate);
-
-module.exports = tesRoutes;
